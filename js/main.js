6163f7049d3e62977ae72e8a8bb99b839f8addc8
Gerrett Rice <gerrettrice@gmail.com>
2017-04-07 18:58:01 -0400

    implement backstretch jQuery plugin

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
-    console.log('jQuery Working');
     $('.owl-carousel').owlCarousel();
+
+    $('.landing').backstretch('https://www.bankogaragedoors.com/content/uploads/2015/10/st-petersburg-fl-41.jpg');
 });
