10290f63a0ff73de428b8e424fc62d78eb3f2d08
Gerrett Rice <gerrettrice@gmail.com>
2017-04-07 19:28:17 -0400

    create custom jQuery interaction

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
     $('.owl-carousel').owlCarousel();
 
     $('.landing').backstretch('https://www.bankogaragedoors.com/content/uploads/2015/10/st-petersburg-fl-41.jpg');
+
+
+    //Clicking the h3s in the resume section will change their highlight color to red
+    $('h3').click(function () {
+        $('h3').css(
+            'background', 'red'
+        );
+    });
 });
