46c47e73d21ecdb59414d0dd32c33b71a9e91f20
Gerrett Rice <gerrettrice@gmail.com>
2017-04-07 20:55:15 -0400

    add image files to git repo

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
+
+    //Owl Carousel - jQuery Plugin
     $('.owl-carousel').owlCarousel();
 
+    //Backstretch jQuery PLugin
     $('.landing').backstretch('https://www.bankogaragedoors.com/content/uploads/2015/10/st-petersburg-fl-41.jpg');
 
-
-    //Clicking the h3s in the resume section will change their highlight color to red
+    //Clicking the h3s in the resume section will change their background color to red
     $('h3').click(function () {
         $('h3').css(
             'background', 'red'
